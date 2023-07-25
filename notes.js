72fc03be0ca5cb1b61493b2f74d26ede89eee04e
tich <tzvidzie@gmail.com>
2023-07-25 14:30:16 +0200

    node

--- notes.js
+++ notes.js
@@ -1,2 +1,19 @@
 const i =0;
 // node.js
+
+/*
+  Node.js is a powerful tool for controlling servers, 
+building web applications, and creating event-driven
+  programs. And it takes JavaScript—a language familiar
+to all web developers—out of the browser. With Node.js,
+  you can build applications that run on your laptop or 
+ even the cloud. In this course, learn the essentials 
+of Node.js and start creating your own JavaScript applications. 
+ Instructor Eve Porcello covers all of the basics, showing how to
+      install Node.js and work with the Node.js core, which includes standard input,
+ standard output, the module system, the file system, and how to write and run 
+JavaScript on the server. Upon wrapping up this course, 
+you’ll be equipped with fundamental Node.js concepts and techniques
+that you can put to use in your next project.
+
+*/
