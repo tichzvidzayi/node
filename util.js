25f40aa74942491504ef1baaf80f906d5d4247e0
tich <tzvidzie@gmail.com>
2023-07-26 12:05:38 +0200

    v8

--- util.js
+++ util.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const util = require("util");
-
+const v8 =  require("v8"); // we can get how much memory our app is using
 console.log(path.basename(__filename));
 const dirUploads = path.join(
 __dirname,
